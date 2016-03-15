91056342ae54293a980a308141795a26695666f7
M4thG33k <pepethemathgeek@gmail.com>
2016-03-14 23:09:08 -0500

    first commit

---
+++ src/main/java/com/m4thg33k/gemulation/Gemulation.java
@@ -0,0 +1,12 @@
+package com.m4thg33k.gemulation;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = Gemulation.MODID, version = Gemulation.VERSION)
+public class Gemulation {
+
+    public static final String MODID = "gemulation";
+    public static final String VERSION = "1.0";
+
+
+}
