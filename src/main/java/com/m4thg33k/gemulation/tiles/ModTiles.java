351d525ecd42bad222289d0ecc3f69d79a8094f9
M4thG33k <pepethemathgeek@gmail.com>
2016-03-19 18:03:10 -0500

    Starting work on Gem Changer

--- src/main/java/com/m4thg33k/gemulation/tiles/ModTiles.java
+++ src/main/java/com/m4thg33k/gemulation/tiles/ModTiles.java
@@ -9,5 +9,6 @@ public class ModTiles {
     {
         String prefix = "tile.gemulation:";
         GameRegistry.registerTileEntity(TileGemFurnace.class, prefix + Names.GEM_FURNACE);
+        GameRegistry.registerTileEntity(TileGemChanger.class, prefix + Names.GEM_CHANGER);
     }
 }
---
+++ src/main/java/com/m4thg33k/gemulation/tiles/TileGemChanger.java
@@ -0,0 +1,6 @@
+package com.m4thg33k.gemulation.tiles;
+
+import net.minecraft.tileentity.TileEntity;
+
+public class TileGemChanger extends TileEntity {
+}
